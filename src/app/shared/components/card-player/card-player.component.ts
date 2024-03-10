fcde569bff0fc838e6f4e5c8f8210fe0a7b61041
gonzalocasquete <gonzalocasquete2310@gmail.com>
2024-03-10 19:04:24 +0100

    Completando visualmente ventana de tracks

--- src/app/shared/components/card-player/card-player.component.ts
+++ src/app/shared/components/card-player/card-player.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { TrackModel } from 'src/app/core/models/tracks.models';
 
 @Component({
   selector: 'app-card-player',
   templateUrl: './card-player.component.html',
   styleUrl: './card-player.component.css'
 })
-export class CardPlayerComponent {
-
+export class CardPlayerComponent implements OnInit {
+  @Input() mode: 'small' | 'big' = 'small'
+  @Input() track!: TrackModel;
+  constructor(){}
+  ngOnInit() : void{ }
 }
--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TrackModel } from 'src/app/core/models/tracks.models';
 
 @Component({
   selector: 'app-media-player',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './media-player.component.css'
 })
 export class MediaPlayerComponent implements OnInit {
-  mockCover:any = {
-    cover:'https://i.scdn.co/image/ab67616d0000b27345ca41b0d2352242c7c9d4bc',
-    album:'Gioli & Assia',
-    name: 'BEBE (Oficial)'
+  mockCover: TrackModel = {
+    cover: 'https://i.scdn.co/image/ab67616d0000b27345ca41b0d2352242c7c9d4bc',
+    album: 'Gioli & Assia',
+    name: 'BEBE (Oficial)',
+    url: '',
+    _id: 1
   }
   constructor(){}
 
