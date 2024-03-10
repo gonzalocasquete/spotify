cb10cd70596675e7e64729b8e1a946c3551da990
gonzalocasquete <gonzalocasquete2310@gmail.com>
2024-03-10 13:42:45 +0100

    Recorrido de tracks

--- src/app/modules/tracks/tracks.module.ts
+++ src/app/modules/tracks/tracks.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { TrackPageComponent } from './pages/track-page/track-page.component';
 import { TracksRoutingModule } from './tracks-routing.module';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TrackPageComponent
+  ],
   imports: [
     CommonModule,
-    TracksRoutingModule
-  ]
+    TracksRoutingModule,
+    SharedModule
+  ],
+ 
 })
 export class TracksModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
 import { MediaPlayerComponent } from '../shared/components/media-player/media-player.component';
 import { HeaderUserComponent } from '../shared/components/header-user/header-user.component';
+import { CardPlayerComponent } from '../shared/components/card-player/card-player.component';
+import { SectionGenericComponent } from '../shared/components/section-generic/section-generic.component';
 
 
 @NgModule({
   declarations: [
     SideBarComponent,
     MediaPlayerComponent,
-    HeaderUserComponent
+    HeaderUserComponent,
+    CardPlayerComponent,
+    SectionGenericComponent
   ],
   imports: [
     CommonModule
@@ -17,7 +21,9 @@ import { HeaderUserComponent } from '../shared/components/header-user/header-use
   exports: [
     SideBarComponent,
     MediaPlayerComponent,
-    HeaderUserComponent
+    HeaderUserComponent,
+    CardPlayerComponent,
+    SectionGenericComponent
   ]
 })
 export class SharedModule { }
